c0d30d2f63e33a603d12684bbaef001d3a9f3a13
William George <wgcreative@protonmail.com>
2022-01-26 02:18:14 -0500

    Add test, run and read failure error, develop to make red green (test pass based on TDD

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,17 +1,17 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "should get home page for static_pages controller" do
     get static_pages_home_url
     assert_response :success
   end
 
-  test "should get help" do
+  test "should get help page for static_pages controller" do
     get static_pages_help_url
     assert_response :success
   end
 
-  test "should get about" do  
+  test "should get about page for static_pages controller" do  
     get static_pages_about_url
     assert_response :success
   end
