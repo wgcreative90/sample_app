5aafe053f286e0938167e82505d44cb9cf595157
William George <wgcreative@protonmail.com>
2022-01-26 03:03:42 -0500

    Added page title specs for home help and about routes with test returning RED on title checks or each route

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home page for static_pages controller" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help page for static_pages controller" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get about page for static_pages controller" do  
+  test "should get about page for static_pages controller" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
