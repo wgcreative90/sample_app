4e21d9243fab318a8b01ea107e2b28a7789e1a4b
William George <wgcreative@protonmail.com>
2022-01-26 03:48:19 -0500

    TDD: Titles for each action's, Write tests => RED, Write Code to turn test green for page titles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/help'
-  root 'static_pages/hello'
+  root 'application#hello'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
-require "test_helper"
-
+require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home page for static_pages controller" do
+ 
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'should get home page for static_pages controller' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "home | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Home | #{@base_title}"
   end
 
-  test "should get help page for static_pages controller" do
+  test 'should get help page for static_pages controller' do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "Help | #{@base_title}"
   end
 
-  test "should get about page for static_pages controller" do
+  test 'should get about page for static_pages controller' do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', "About | #{@base_title}"
   end
 end
