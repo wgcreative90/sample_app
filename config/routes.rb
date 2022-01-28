17c5ab5cc6046cbf21e91e77ec1e5d6189743440
William George <wgcreative@protonmail.com>
2022-01-28 14:50:08 -0500

    Cleanup formatting and route placement, passing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'  # GET /static/pages/home
-  get 'static_pages/about' # GET /static/pages/about
-  get 'static_pages/help'  # GET /static/pages/help
   root 'static_pages#home' # GET /root => /static/home
+    get 'static_pages/home'  # GET /static/pages/home
+    get 'static_pages/about' # GET /static/pages/about
+    get 'static_pages/help'  # GET /static/pages/help
 end
