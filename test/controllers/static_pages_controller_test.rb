f86859c0570b38274a0d5b79becd23a0b56f9c8b
William George <wgcreative@protonmail.com>
2022-02-02 12:28:01 -0500

    Add: full_title helper - adjust code to turn tests green.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,7 +13,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home page for static_pages controller' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+
   end
 
   test 'should get help page for static_pages controller' do
