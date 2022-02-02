07765d71113179ac43a87bb6de79535609ed7157
William George <wgcreative@protonmail.com>
2022-02-02 13:18:46 -0500

    Update: all titles with helper: test's green

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,19 +13,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home page for static_pages controller' do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", @base_title
 
   end
 
   test 'should get help page for static_pages controller' do
     get static_pages_help_url
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', @base_title
   end
 
   test 'should get about page for static_pages controller' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', @base_title
   end
 end
