aec5c63872c4a9999ddd46d5f1f7e7e6e289659e
William George <wgcreative@protonmail.com>
2022-03-02 17:22:24 -0500

    Add routes, structure, and styling to sample application,also fixed guard test runner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
+# Application helper methods module for customer helper methods
 module ApplicationHelper
+
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = 'Ruby on Rails Tutorial Sample App'
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      "#{page_title} | #{base_title}"
     end
   end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # Home or Root path
   root 'static_pages#home' # GET /root => /static/home
-    get 'static_pages/home'  # GET /static/pages/home
-    get 'static_pages/about' # GET /static/pages/about
-    get 'static_pages/help'  # GET /static/pages/help
+
+  # Static page paths for nav bar
+  get 'static_pages/home'  # GET /static/pages/home
+  get 'static_pages/about' # GET /static/pages/about
+  get 'static_pages/help'  # GET /static/pages/help
+  get 'static_pages/login'  # GET /static/pages/help
+
 end
