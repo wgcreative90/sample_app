8bfdd0f09ef63c54e2b3026c34ecb470681ca8ff
William George <wgcreative@protonmail.com>
2022-03-09 05:13:56 -0500

    drop dev db and update seed file for pw and pw_confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'Foo', email: 'fooBarFoo@bar.com')
-User.create(name: 'Mother', email: 'anotherMother@example.org')
+User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
+User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
