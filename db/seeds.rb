1aae6520891f4aa59063bba685f5ce7aceecf939
William George <wgcreative@protonmail.com>
2022-03-09 06:25:30 -0500

    password digest and bcrypt configs and explantion

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-
-
-#   run rake db:drop , rake db:create, rake db:seed, rails c, User.count => 1, this is because only one user below
-# passes validations in the model and model/user/user_test.rb specs are all passing so we knew this would happen
-# already thanks to TDD test driven development.
-User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
+#
+#
+#
+User.new( name: "bizzbuzz", email: "bizz@buzz.com", password_digest: "bizzbuzz", password_confirmation: "bizzbuzz" )
+binding.pry
 
 # mathces above user INVALID => rollback in db
-User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
+# User.create(name: "Michael Hartl", email: "michael@example.com", password_digest: "foobar", password_confirmation: "foobar")
 
 # password: < 6, INVALID => rollback in db on attempt to save to sqlite3 via activeRecord
-User.create(name: "Michael H", email: "michaelexample.com", password: "foo", password_confirmation: "foobar")
+# User.create(name: "Michael H", email: "michaelexample.com", password_digest: "foo", password_confirmation: "foobar")
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: 'Example User',
+                     email: 'user@example.com',
+                     password: 'foobar',
+                     password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -29,26 +30,26 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test 'email addresses should be unique' do
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lowercase" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
+  test 'email addresses should be saved as lowercase' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+  test 'password should be present (not blank)' do
+    @user.password = @user[:password_confirmation] = ' ' * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+  test 'password should have a minimum length' do
+    @user.password = @user[:password_confirmation] = 'a' * 5
     assert_not @user.valid?
   end
 
