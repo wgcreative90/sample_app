edcbc6ba0079bc4ade5672d445fdbe5d39494052
William George <wgcreative@protonmail.com>
2022-03-09 05:20:41 -0500

    Updated seed and added comments and an exercises to understand the validations better and why they are working and how we cant test this through a simple seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,15 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+#   run rake db:drop , rake db:create, rake db:seed, rails c, User.count => 1, this is because only one user below
+# passes validations in the model and model/user/user_test.rb specs are all passing so we knew this would happen
+# already thanks to TDD test driven development.
 User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
+
+# mathces above user INVALID => rollback in db
 User.create(name: "Michael Hartl", email: "michael@example.com", password: "foobar", password_confirmation: "foobar")
+
+# password: < 6, INVALID => rollback in db on attempt to save to sqlite3 via activeRecord
+User.create(name: "Michael H", email: "michaelexample.com", password: "foo", password_confirmation: "foobar")
