2f419f2996cc6a6c63029f7432eeaa402ca542cd
William George <wgcreative@protonmail.com>
2022-03-11 15:22:34 -0500

    remove name from schema:

---
+++ db/migrate/20220311201427_remove_name_column_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveNameColumnFromUser < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_095558) do
+ActiveRecord::Schema.define(version: 2022_03_11_201427) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
