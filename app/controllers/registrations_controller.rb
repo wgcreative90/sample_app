995808ba6a63ef6a214bcd7271ac451f523f632d
William George <wgcreative@protonmail.com>
2022-03-11 11:25:38 -0500

    refactor using sessions controller and registrations controller and change routes -WIP: This will not work yet as the routes need to be changed in multiple places and other files moved or ripped out. -WHY: I Think this is going to be a faster approach and cleaner approach vs the previous implimentation which was confusing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@
     def new
       @user = User.new
     end
+
     def create
       @user = User.new(user_params)
       if @user.save
@@ -13,9 +14,12 @@
         render :new
       end
     end
+
     private
+
     def user_params
       # strong parameters
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
+
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,30 @@
+# Rails.application.routes.draw do
+#   root   'welcome#index'
+#   get    '/help',    to: 'static_pages#help'
+#   get    '/about',   to: 'static_pages#about'
+#   get    '/contact', to: 'static_pages#contact'
+#   get    '/signup',  to: 'users#new'
+#   post   '/signup',  to: 'session#new'
+#   get    '/login',   to: 'users#login'
+#   post   '/login',   to: 'sessions#create'
+#   delete '/logout',  to: 'sessions#destroy'
+#   resources :users
+#   resources :account_activations, only: [:edit]
+
+  
+# end
+
 Rails.application.routes.draw do
-  root   'welcome#index'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'session#new'
-  get    '/login',   to: 'users#login'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :users
-  resources :account_activations, only: [:edit]
+  root 'welcome#index'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
+  delete 'logout', to: 'sessions#destroy'
+  get 'password', to: 'passwords#edit', as: 'edit_password'
+  patch 'password', to: 'passwords#update'
+  get 'password/reset', to: 'password_resets#new'
+  post 'password/reset', to: 'password_resets#create'
+  get 'password/reset/edit', to: 'password_resets#edit'
+  patch 'password/reset/edit', to: 'password_resets#update'
 end
