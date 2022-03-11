a63b720f9bc27ea1e91b0f7a62971c93d3cd64b5
William George <wgcreative@protonmail.com>
2022-03-11 18:37:10 -0500

    login or signup working but styling needs to be adjusted if time

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
-class User < ActiveRecord::Base
-  # attr_accessor :name, :email, :password_digest, :password_confirmation
+class User < ApplicationRecord
+  # adds virtual attributes for authentication
   has_secure_password
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :email,presence: true,length: { maximum: 255 }, uniqueness: { case_sensitive: false }
-
-  validates :email,
-            presence: true,
-            length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+  # validates email
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'welcome#index', as: 'landing_path'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
   get 'sign_in', to: 'sessions#new'
@@ -13,8 +13,9 @@ Rails.application.routes.draw do
   patch 'password/reset/edit', to: 'password_resets#update'
 
   get    'help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
-  resources :users
+  get    'about',   to: 'static_pages#about'
+  get    'contact', to: 'static_pages#contact'
+
+  resources :users, only: %i[new create destroy edit update]
   resources :account_activations, only: [:edit]
 end
