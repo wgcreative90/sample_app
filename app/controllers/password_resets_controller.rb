2f14a599445440d66bcb512850b17c901066869b
William George <wgcreative@protonmail.com>
2022-03-13 21:25:01 -0400

    Update/Add: config.action_mailer for user password reset via email recovery.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,6 +10,16 @@ class PasswordResetsController < ApplicationController
         redirect_to sign_in_path, alert: 'Your token has expired. Please try again.'
     end
 
+    def create
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      # send mail
+      PasswordMailer.with(user: @user).reset.deliver_later
+      # deliver_later is provided by ActiveJob
+    end
+    redirect_to root_path, notice: 'Please check your email to reset the password'
+  end
+
     def update
       # updates user's password
       @user = User.find_signed!(params[:token], purpose: 'password_reset')
