068fd0113b1ac5b22af5f1a0d0e92c71aac7d9ab
William George <wgcreative@protonmail.com>
2022-03-13 22:28:22 -0400

    apc syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]; end
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id]) 
+      Current.user
+    end
   end
 
   def require_user_logged_in!
     # allows only logged in user
-    redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?; end
+    if Current.user.nil?
+      redirect_to sign_in_path, alert: 'You must be signed in' 
+    end
+    
   end
 
 end
