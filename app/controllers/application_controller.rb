9acb10a047bf36a1c36c568f98193a4c990f4b95
William George <wgcreative@protonmail.com>
2022-03-13 22:50:50 -0400

    syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,17 @@
+
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
 
-  before_action :set_current_user
-
-  def set_current_user
-    if session[:user_id]
-      Current.user = User.find_by(id: session[:user_id]) 
-      Current.user
+    def set_current_user
+      if session[:user_id]
+        Current.user = User.find_by(id: session[:user_id])
+      end
     end
-  end
 
-  def require_user_logged_in!
-    # allows only logged in user
-    if Current.user.nil?
-      redirect_to sign_in_path, alert: 'You must be signed in' 
+    def require_user_logged_in!
+      # allows only logged in user
+      if Current.user.nil?
+        redirect_to sign_in_path, alert: 'You must be signed in' 
+      end
     end
-    
-  end
-
 end
