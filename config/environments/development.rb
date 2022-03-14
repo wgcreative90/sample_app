9530485d1e4a15420a9c87c9e2576709417b78f9
William George <wgcreative@protonmail.com>
2022-03-13 22:18:48 -0400

    update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,22 +4,24 @@ Rails.application.configure do
   config.cache_classes = false
 
   # ActiveSupport for Rails meailer password reset.
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000',  protocol: 'http' }
+  # config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['email'],
-    password:       ENV['password'],
-    domain:         ENV['localhost:3000'],
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      ENV['email'],
+  #   password:       ENV['password'],
+  #   domain:         ENV['localhost:3000'],
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
