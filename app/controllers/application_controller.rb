b9a12080bfa4364b3cbc7d1828a9cf7b2ac37375
William George <wgcreative@protonmail.com>
2022-03-13 22:24:47 -0400

    rake action_mailbox:install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
+
   before_action :set_current_user
 
   def set_current_user
-    # finds user with session data and stores it if present
-    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]; end
   end
 
   def require_user_logged_in!
     # allows only logged in user
-    redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?
+    redirect_to sign_in_path, alert: 'You must be signed in' if Current.user.nil?; end
   end
+
 end
